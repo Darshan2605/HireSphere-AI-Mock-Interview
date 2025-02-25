24f2993fad6e0fc521858377bff1dfae4f9702c1
Darshan Londhe <116908065+Darshan2605@users.noreply.github.com>
2025-02-26 00:32:53 +0530

    DashBoard Link Updated

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,22 +1,24 @@
 "use client"
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 export const Header = () => {
-    const path=usePathname()
-    useEffect(()=>{
+    const path = usePathname()
+    useEffect(() => {
         console.log(path);
     })
-  return (
-    <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'/HireSp.png'} alt='logo' width={100} height={50}  style={{ width: "auto", height: "auto" , borderRadius: "10%" }}   />
-        <ul className='hidden md:flex gap-6'>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-green-900 font-bold'}`}>Dashboard</li>
-            
-        </ul>
-        <UserButton/>
-    </div>
-  )
+    return (
+        <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
+            <Image src={'/HireSp.png'} alt='logo' width={100} height={50} style={{ width: "auto", height: "auto", borderRadius: "10%" }} />
+            <ul className='hidden md:flex gap-6'>
+                <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path == '/dashboard' && 'text-green-900 font-bold'}`}>
+                    <Link href="/dashboard">Dashboard</Link>
+                </li>
+            </ul>
+            <UserButton />
+        </div>
+    )
 }
