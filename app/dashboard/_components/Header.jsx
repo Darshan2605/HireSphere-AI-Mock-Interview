1dcc19228a3aa6c98a8edf780ca44ee975ed0d17
Darshan Londhe <116908065+Darshan2605@users.noreply.github.com>
2025-02-26 00:29:25 +0530

    logo Changed

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -11,9 +11,9 @@ export const Header = () => {
     })
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'/Hire1.png'} alt='logo' width={160} height={100}  style={{ width: "auto", height: "auto" }}   />
+        <Image src={'/HireSp.png'} alt='logo' width={100} height={50}  style={{ width: "auto", height: "auto" , borderRadius: "10%" }}   />
         <ul className='hidden md:flex gap-6'>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-green-900 font-bold'}`}>Dashboard</li>
             
         </ul>
         <UserButton/>
